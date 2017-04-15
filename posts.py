a0433bc198db2573d22bda69ea6b19932ff7a232
GreStas <grestas2000@gmail.com>
2017-04-15 20:33:21 +0300

    Создание объекта для поста #4
    Вернул к версии с чётко определённым набором атрибутов экспорта-импорта вместо использования __dict__ так как возникло понимание, что со временем будут приватные атрибуты, которые не надо будет экспортировать.
    
    Определился с исползованием атрибута timestamp:
    - при загрузке из источника или обновлении из источника должен обновляться
    - при экспорте-импорте должен сохранять оригинальное значение
    
    Так как json требует сериализации, то экспорт-импорт в-из словаря преобразоввывает даты в строки по стандартному для модуля формату - глобальная переменная модуля datetime_format - можно импортитровать.
    
    Исправлено несколько багов.

--- posts.py
+++ posts.py
@@ -26,24 +26,40 @@ import datetime
 #         else:
 #             raise TypeError('Parameter is not Post')
 
+# единый стандарт для преобразования datetime для совместимости с serialise (например JSON)
+datetime_format = "%Y%m%d:%H:%M:%S%f"
 
 def set_timestamp(f):
+    """ Декоратор для автоматического обновления аттрибута timestamp
+    Ограничения:
+    - можно декорировать только методы классов, у которых есть аатрибут timestamp
+    - нежелательно применять, метод изменяет аттрибут timestamp"""
     def wrapper(self, *args, **kwargs):
         result = f(self, *args, **kwargs)
-        self.timestamp = datetime.datetime.now()
+        self._timestamp = datetime.datetime.utcnow()
         return result
     return wrapper
 
 
 class Post:
-    def __init__(self, date, url, body,):
-        self.import_dict(
-            {
-                'date': date,
-                'url': url,
-                'body': body,
-            }
-        )
+    """ Контейнер, хранящий данные о публикации.
+    Одно из применений - лента новостей, которая может быть организована как список объектов класса Post
+    или производных от него классов.
+    Post создаётся из источника каким-нибудь загрузчиком и этот момент фиксирует в атрибуте timestamp.
+    Post обновляется из источника каким-нибудь загрузчиком и этот момент фиксирует в атрибуте timestamp.
+    import_ рассчитывает на то, что атрибут timestamp уже зафиксировал когда были получены данные
+        при сохранении методом export_ и поэтому не обновляет метку timestamp.
+    """
+    @set_timestamp
+    def __init__(self, date: datetime.datetime, url, body,):
+        self.date = date
+        self.url = url
+        self.body = body
+        if __debug__: # продублировано только для удобства работы в pycharm
+            self._timestamp = datetime.datetime.utcnow()
+
+    def __repr__(self):
+        return "%s\n%s\n%s\n%s\n" % (self.date, self.url, self.body, self._timestamp)
 
     @set_timestamp
     def update(self, body):
@@ -51,29 +67,31 @@ class Post:
         self.body = body
 
     def export_dict(self):
-        """Формирует словарь аттрибутов для дальнейшего хранения"""
-        return {
-            'date': self.date,
+        """Возвращает словарь выборочных! аттрибутов для дальнейшего хранения"""
+        data = {
+            'date': self.date.strftime(datetime_format),
             'url': self.url,
             'body': self.body,
-            'timestamp': self.timestamp
+            'timestamp': self._timestamp.strftime(datetime_format)
         }
-        #return self.__dict__.copy()
+        return data
 
-    def export_(self, fmt: str):
-        """ Возвращает строку в заданном формате
-        для начала в JSON"""
-        if fmt == 'JSON':
-            return json.dumps(self.export_dict())
-        raise ValueError("Unknown export format '%s'" % fmt)
-
-    @set_timestamp
     def import_dict(self, data):
-        self.__dict__ = data
+        self.date = datetime.datetime.strptime(data['date'],datetime_format)
+        self.url = data['url']
+        self.body = data['body']
+        self._timestamp = datetime.datetime.strptime(data['timestamp'],datetime_format)
 
-    def import_(self, fmt: str, data: str):
-        """ Втяшиваем строку в заданном формате
-        для начала в JSON"""
-        if fmt == 'JSON':
-            self.import_dict(json.loads(data))
-        raise ValueError("Unknown import format '%s'" % fmt)
+if __name__ == '__main__':
+    _today = datetime.date.today()
+    p = Post(_today, "https://", "Bla-bla-bla")
+    print(p)
+    ps = p.export_dict()
+    print(ps)
+    p.import_dict(ps)
+    print(p)
+
+    ps = p.export_dict()
+    print(ps)
+    p.import_dict(ps)
+    print(p)
